a2a36270ed4253c165f12a2c781e5ceded927603
shamm92 <39699303+shamm92@users.noreply.github.com>
2018-05-28 14:12:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace EmpaticaBLEClient
+{
+    // State object for receiving data from remote device.
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.FirstOrDefault() == "--help")
+            {
+                Console.WriteLine();
+                Console.WriteLine("********** Empatica BLE Client **********");
+                Console.WriteLine("For the communication protocol and the list of commands, please visit");
+                Console.WriteLine("http://empatica.github.io/windows-ble-server.html");
+                return;
+            }
+
+            Console.WriteLine("Press ENTER to connect to Empatica BLE Server");
+            Console.ReadKey();
+
+            AsynchronousClient.StartClient();
+
+            Console.ReadKey();
+        }
+    }
+}
